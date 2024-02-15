82ac4f7e09b82815a720bf95108a6b86a323d774
serega-k <zloebuch@gmail.com>
2024-02-15 06:30:18 +0300

    feat(downloding): comparision with latest remote version and download if it's needed (SP-3913)

--- src/services/download/index.ts
+++ src/services/download/index.ts
@@ -1,20 +1,29 @@
 import * as fs from 'fs';
+import semver from 'semver';
 import axios from 'axios';
 import { getOSAndArch } from './utils';
 import { ILogger } from '../../common/logger';
 import * as Progress from '../../common/progress';
-import { SPCTL_DOWNLOAD_URL_PREFIX } from '../../common/constant';
+import { SPCTL_LATEST_RELEASE_URL, SPCTL_MIN_COMPATIBLE_VERSION } from '../../common/constant';
 import { PathLike } from 'fs-extra';
 import { createSpctlService } from '../spctl';
 import { ConfigLoader } from '../../common/loader.config';
 
 const DOWNLOADING_PROGRESS = 'Downloading spctl';
 
-type DownloadSpctlParams = Omit<CheckAndDownloadSpctlParams, 'configLoader'>;
+const VERSION_MATCH = '{{version}}';
+const OS_MATCH = '{{os}}';
+const ARCH_MATCH = '{{arch}}';
+
+const SPCTL_DOWNLOAD_URL_TEMPLATE = `https://github.com/Super-Protocol/ctl/releases/download/v${VERSION_MATCH}/spctl-${OS_MATCH}-${ARCH_MATCH}`;
+
+type DownloadSpctlParams = Omit<CheckAndDownloadSpctlParams, 'configLoader'> & { version: string };
 const downloadSPCTL = async (params: DownloadSpctlParams): Promise<void> => {
   const { logger, destination } = params;
   const osAndArch = getOSAndArch();
-  const url = SPCTL_DOWNLOAD_URL_PREFIX + osAndArch;
+  const url = SPCTL_DOWNLOAD_URL_TEMPLATE.replace(VERSION_MATCH, params.version)
+    .replace(OS_MATCH, osAndArch.platform)
+    .replace(ARCH_MATCH, osAndArch.arch);
   const file = fs.createWriteStream(destination);
 
   try {
@@ -60,6 +69,7 @@ export type CheckAndDownloadSpctlParams = {
 export const checkAndDownloadSpctl = async (params: CheckAndDownloadSpctlParams): Promise<void> => {
   const { logger, destination } = params;
   try {
+    let currentVersion = '';
     if (fs.existsSync(destination)) {
       logger?.trace(
         `spctl already exists at ${destination}. Making comparison with latest version...`,
@@ -68,19 +78,73 @@ export const checkAndDownloadSpctl = async (params: CheckAndDownloadSpctlParams)
         logger,
         config: params.configLoader,
       });
-      const version = await service.getVersion();
-      logger?.info(`spctl version ${version} will be used`); // TODO: if needed to make update to latest version
-    } else {
-      logger?.debug(`Downloading spctl to ${destination}`);
-      await downloadSPCTL(params).catch((err) => {
+      currentVersion = (await service.getVersion()).replace('\n', '');
+    }
+    const updateInfo = await hasNewVersion(currentVersion);
+    if (updateInfo.hasNewVersion && updateInfo.version && isCompatible(updateInfo.version)) {
+      logger?.debug(`Downloading spctl v.${updateInfo.version} to ${destination}`);
+      await downloadSPCTL({
+        logger: params.logger,
+        destination: params.destination,
+        version: updateInfo.version,
+      }).catch((err) => {
         logger?.error({ err }, 'Failed to check or download spctl');
       });
 
       logger?.debug(`Successfully downloaded spctl to ${destination}`);
+    } else if (!currentVersion) {
+      logger?.error(
+        'Resource for downloading was not found and you do not have any downloaded spctl-tool version yet. The program will be closed.',
+      );
+      process.exit(0);
+    } else if (!isCompatible(currentVersion)) {
+      logger?.error(
+        `Installed spctl v${currentVersion}is not compatible with v${SPCTL_MIN_COMPATIBLE_VERSION}`,
+      );
+      process.exit(0);
     }
   } catch (err) {
     logger?.error({ err }, 'Failed to check or download spctl');
 
     throw err;
+  }
+};
+
+const isCompatible = (version: string): boolean => {
+  const checked = semver.clean(version);
+  const compatible = semver.clean(SPCTL_MIN_COMPATIBLE_VERSION);
+
+  if (!(checked && compatible)) {
+    return false;
+  }
+
+  return semver.gte(checked, compatible);
+};
+
+type GasNewVersionReturnType = {
+  hasNewVersion: boolean;
+  version?: string;
+};
+export const hasNewVersion = async (currentVersion?: string): Promise<GasNewVersionReturnType> => {
+  try {
+    const response = await axios.get(SPCTL_LATEST_RELEASE_URL);
+    const latestVersion = semver.clean(response.data.tag_name);
+
+    if (!latestVersion) {
+      return {
+        hasNewVersion: false,
+        version: currentVersion,
+      };
+    }
+
+    return {
+      hasNewVersion: !currentVersion || semver.gt(latestVersion, currentVersion),
+      version: latestVersion,
+    };
+  } catch (error) {
+    return {
+      hasNewVersion: false,
+      version: currentVersion,
+    };
   }
 };
--- src/services/download/utils.ts
+++ src/services/download/utils.ts
@@ -1,15 +1,17 @@
-export const supportedPlatform: NodeJS.Platform[] = ['darwin', 'linux'];
+export const supportedPlatform: NodeJS.Platform[] = ['darwin', 'linux', 'win32'];
 export const supportedArch: NodeJS.Architecture[] = ['arm64', 'x64'];
-export const getOSAndArch = (): string => {
-  const os = process.platform;
-  if (!supportedPlatform.includes(os)) {
-    throw new Error(`Unsupported OS: ${os}`);
+export const getOSAndArch = (): { platform: string; arch: string } => {
+  const platform = process.platform;
+  if (!supportedPlatform.includes(platform)) {
+    throw new Error(`Unsupported OS: ${platform}`);
   }
-
   const arch = process.arch;
   if (!supportedArch.includes(arch)) {
     throw new Error(`Unsupported arch: ${arch}`);
   }
 
-  return `${os}-${arch}`;
+  return {
+    platform: platform === 'darwin' ? 'macos' : platform,
+    arch,
+  };
 };
