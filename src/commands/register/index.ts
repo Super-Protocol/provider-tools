54315d1f11b505480c9f2d7f9f8d49098c123565
Dmitry Somov <smv.dmitry@gmail.com>
2024-02-20 11:32:24 +0300

    feat(register): support --value option (SP-3751)

--- src/commands/register/index.ts
+++ src/commands/register/index.ts
@@ -10,6 +10,7 @@ import buildDeployConfig from './buildDeployConfig';
 
 type CommandParams = ConfigCommandParam & {
   tee: boolean;
+  value: boolean;
   backendUrl: string;
   blockchainUrl: string;
   contractAddress: string;
@@ -20,15 +21,16 @@ const logger = createLogger().child({ command: COMMAND_NAME });
 
 export const RegisterCommand = new Command()
   .name(COMMAND_NAME)
-  .description('register tee-provider')
-  .requiredOption('--tee', 'specified type of provider', false)
+  .description('register provider')
+  .option('--tee', 'tee type of provider', false)
+  .option('--value', 'value type of provider', false)
   .option('--backend-url <url>', 'backend url')
   .option('--blockchain-url <url>', 'blockchain url')
   .option('--contract-address <address>', 'contract address')
   .action(async (options: CommandParams): Promise<void> => {
-    if (!options.tee) {
+    if (!options.tee || !options.value) {
       return logger.error(
-        'At least one of supported provider types should be specified. Please try to run command again and have specified the "--tee" param.',
+        'Please specify at least one of the supported provider types by including either the --tee or --value option when you run the command again.',
       );
     }
     const config = new ConfigLoader(options.config);
