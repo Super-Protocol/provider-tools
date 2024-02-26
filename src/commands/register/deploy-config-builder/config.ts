d19d85b6487c227679f41e7e14dfd97f1a095818
serega-k <zloebuch@gmail.com>
2024-02-26 13:03:53 +0300

    feat(registerCommand): format deploy-config.yaml file is changed by devops requirements (SP-3913)

--- src/commands/register/deploy-config-builder/config.ts
+++ src/commands/register/deploy-config-builder/config.ts
@@ -1,11 +1,5 @@
 export const deployConfig = {
-  apiVersion: 'v1',
-  kind: 'ConfigMap',
-  metadata: {
-    name: 'golden-execution-controller-tee-prov',
-  },
   data: {
-    APP_NAME: 'execution-controller-tee',
     PROVIDER_ACTION_ACCOUNT_KEY: '{{ action-account-private-key }}',
     PROVIDER_AUTH_ADDRESS: '{{ authority-account-address }}',
     PROVIDER_OFFERS_JSON: [
