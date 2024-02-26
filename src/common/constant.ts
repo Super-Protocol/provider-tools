c4c098c268abd0b0c7bcba4c4b4bd0202d761aaf
Dmitry Somov <smv.dmitry@gmail.com>
2024-02-26 12:51:52 +0300

    feat(register): support download runner.sh and docker-compose.yaml from Github (SP-3751)

--- src/common/constant.ts
+++ src/common/constant.ts
@@ -44,3 +44,9 @@ export const DEPLOY_CONFIG_PROVIDER_OFFER_DEVICE_ID = '{{ device-id }}';
 export const MILLISECONDS_IN_DAY = 24 * 60 * 60 * 1000;
 export const DEFAULT_DEPLOY_DESTINATION_FOLDER_PATH = '/sp/manifests';
 export const MB_TO_BYTES_MULTIPLIER = 1000000;
+export const RUNNER_SH_URL =
+  process.env.RUNNER_SH_URL ||
+  'https://raw.githubusercontent.com/Super-Protocol/provider-tools/main/runner-assets/runner.sh';
+export const DOCKER_COMPOSE_URL =
+  process.env.DOCKER_COMPOSE_URL ||
+  'https://raw.githubusercontent.com/Super-Protocol/provider-tools/main/runner-assets/docker-compose.yaml';
