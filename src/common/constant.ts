d82ac2c13a61d80a244c7ccf1595cd3d584765d0
serega-k <zloebuch@gmail.com>
2024-02-28 19:54:05 +0300

    fix: account private key validator is fixed (SP-3913)

--- src/common/constant.ts
+++ src/common/constant.ts
@@ -10,7 +10,7 @@ export const APP_NAME = name;
 export const APP_VERSION = version;
 export const CONFIG_DEFAULT_FILENAME = path.resolve(execDir(), 'config.json');
 export const JWT_CHECK_REGEX = /(^[A-Za-z0-9-_]*\.[A-Za-z0-9-_]*\.[A-Za-z0-9-_]*$)/;
-export const PRIVATE_KEY_CHECK_REGEX = /\b(?:0x)?[0-9a-fA-F]{64}\b/g;
+export const PRIVATE_KEY_CHECK_REGEX = /^(?:0x)?[0-9a-fA-F]{64}$/;
 export const SPCTL_BACKEND_URL_DEFAULT =
   process.env.NODE_ENV === 'production'
     ? 'https://bff.testnet.superprotocol.com/graphql'
